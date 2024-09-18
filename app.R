49836946f09145244b07b83df9df94f912dac4b8
hanguojun007 <2867965897@qq.com>
2024-09-18 22:49:03 +0800

    add 20240918

--- app.R
+++ app.R
@@ -1,74 +1,30 @@
 library(shiny)
-library(reactable)
-library(ComplexHeatmap)
+
+source("/root/shiny/shinyApps/DiffAnalysis/test.R")
+source("/root/shiny/shinyApps/Heatmap/test.R")
 
 ui <- fluidPage(
-  titlePanel("Heatmap"),
-  fluidRow(
-    column(
-      4,
-      fileInput("file", label = NULL, placeholder = "Upload File", accept = c("tsv", "csv")),
-      checkboxGroupInput("cols", "Columns", choices = NULL, selected = NULL)
+  navlistPanel(
+    id = "Big Class",
+    widths = c(2, 10),
+    "分析类",
+    tabPanel(
+      "差异分析",
+      "差异分析UI",
+      test_ui()
     ),
-    column(8, reactable::reactableOutput("table"))
-  ),
-  fluidRow(
-    column(
-      4,
-      checkboxInput("show_rownames", "Show Row Names", value = TRUE),
-      checkboxInput("show_colnames", "Show Column Names", value = TRUE)
-    ),
-    column(8, plotOutput("heatmap", click = "plot_click"))
-  ),
-  fluidRow(
-    verbatimTextOutput("info")
+    "画图类",
+    tabPanel(
+      "表达矩阵热图",
+      "表达矩阵热图UI",
+      test2_ui()
+    )
   )
 )
 
 server <- function(input, output, session) {
-  data <- reactive({
-    req(input$file)
-    vroom::vroom(input$file$datapath) |>
-      dplyr::rename(ID = 1) |>
-      tibble::column_to_rownames(var = "ID")
-  })
-
-  output$table <- renderReactable({
-    reactable(data(), paginationType = "jump", selection = "multiple", onClick = "select", defaultSelected = c(1:nrow(data())))
-  })
-  # req(data())
-  selected <- reactive(getReactableState("table", "selected"))
-
-  observeEvent(data(), {
-    updateCheckboxGroupInput(session, "cols", choices = colnames(data()), selected = colnames(data()))
-  })
-
-  col_selected <- reactive({
-    req(input$cols)
-    input$cols
-  })
-
-  showRownames <- reactive(input$show_rownames)
-  showColnames <- reactive(input$show_colnames)
-
-  output$heatmap <- renderPlot(
-    {
-      Heatmap(
-        as.matrix(data()[selected(), col_selected()]),
-        show_row_names = showRownames(),
-        show_column_names = showColnames()
-      )
-    },
-    res = 96
-  )
-
-  output$info <- renderPrint({
-    req(input$plot_click)
-    x <- input$plot_click$x
-    y <- input$plot_click$y
-    cat("[", x, ", ", y, "]", sep = "")
-  })
+  test_server(input, output, session)
+  test2_server(input, output, session)
 }
-
 
 shinyApp(ui, server)
