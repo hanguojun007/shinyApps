5a1fa86d39e63ba95c5a005e3d45cf1a6a81ea4a
hanguojun <2867965897@qq.com>
2024-09-25 15:06:41 +0000

    添加help文档

--- DiffAnalysis/DiffAnalysis.R
+++ DiffAnalysis/DiffAnalysis.R
@@ -5,32 +5,110 @@ source("DiffAnalysis/Test.R")
 
 DiffAnalysis_ui <- function(id) {
   tagList(
-    sidebarLayout(
-      sidebarPanel(
-        fluidPage(
-          fluidRow(UploadFile_ui(NS(id, "diff"), label = "上传文件", multiple = TRUE), style = "margin-bottom: 20px;"),
-          # tags$br(), # 添加间隔
-          fluidRow(SmapleGroup_ui(NS(id, "diff")), style = "margin-bottom: 20px;"),
-          # tags$br(), # 添加间隔
-          fluidRow(CompareMethod_ui(NS(id, "diff")), style = "margin-bottom: 20px;"),
-          # tags$br(), # 添加间隔
-          fluidRow(actionButton(NS(id, "startAnalysis"), "开始差异分析"), style = "margin-bottom: 20px;")
-        ),
-        width = 4
-      ),
-      mainPanel(
-        DataTable_ui(NS(id, "diff")),
-        width = 8
-      )
+    # Add custom CSS for modal customization
+    tags$head(
+      tags$style(HTML("
+      /* 定义用于控制右侧弹出的 modal */
+      #custom-modal .modal-dialog {
+          position: fixed;
+          margin: 0;
+          width: 40%;
+          height: 100%;
+          right: 0;
+          top: 0;
+          bottom: 0;
+          transition: transform 0.3s ease-out;
+      }
+
+      #custom-modal .modal-content {
+          height: 100%;
+          overflow-y: auto;
+      }
+
+      #custom-modal .modal-header, #custom-modal .modal-footer {
+          padding: 10px;
+      }
+
+      /* 遮罩层样式 */
+      .modal-backdrop {
+          background-color: rgba(0,0,0,0.5);
+      }
+
+      /* 初始化时右侧显示外部 */
+      #custom-modal.modal.fade .modal-dialog {
+          transform: translateX(100%);
+      }
+
+      /* 显示时滑入 */
+      #custom-modal.modal.fade.show .modal-dialog {
+          transform: translateX(0);
+      }
+    "))
     ),
-    textOutput(NS(id, "error_message")), # 展示错误信息
-    CompareResult_ui(NS(id, "diff"))
+    fluidPage(
+      fluidRow(actionButton(NS(id, "help"), "Help")), # Help button
+      fluidRow(
+        sidebarLayout(
+          sidebarPanel(
+            fluidPage(
+              fluidRow(UploadFile_ui(NS(id, "diff"), label = "上传文件", multiple = TRUE), style = "margin-bottom: 20px;"),
+              fluidRow(SmapleGroup_ui(NS(id, "diff")), style = "margin-bottom: 20px;"),
+              fluidRow(CompareMethod_ui(NS(id, "diff")), style = "margin-bottom: 20px;"),
+              fluidRow(actionButton(NS(id, "startAnalysis"), "开始差异分析"), style = "margin-bottom: 20px;")
+            ),
+            width = 4
+          ),
+          mainPanel(
+            DataTable_ui(NS(id, "diff")),
+            width = 8
+          )
+        )
+      ),
+      fluidRow(textOutput(NS(id, "error_message"))), # 展示错误信息
+      fluidRow(CompareResult_ui(NS(id, "diff")))
+    )
   )
 }
 
 
 DiffAnalysis_server <- function(id) {
   moduleServer(id, function(input, output, session) {
+    #--------------------------------#
+    # 添加说明文档
+    #--------------------------------#
+    observeEvent(input$help, {
+      showModal(
+        tags$div(
+          id = "custom-modal",
+          modalDialog(
+            tags$head(
+              tags$style(HTML("
+                /* 自定义 Markdown 表格样式 */
+                table {
+                  table-layout: auto; /* 自动调整列宽 */
+                  border-collapse: collapse;
+                  width: auto; /* 表格宽度由内容决定 */
+                  max-width: 100%; /* 防止表格超过容器宽度 */
+                }
+                table, th, td {
+                  border: 1px solid black;
+                }
+                th, td {
+                  padding: 8px;
+                  text-align: left;
+                }
+                th {
+                  background-color: #f2f2f2;
+                }
+              "))
+            ),
+            includeMarkdown("DiffAnalysis/help.md"),
+            easyClose = TRUE
+          )
+        )
+      )
+    })
+
     uploadFileInfo <- ReadFile_server("diff")
 
     #--------------------------------#
